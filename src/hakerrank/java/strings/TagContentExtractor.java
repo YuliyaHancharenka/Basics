71c88eea499b2244709737ac7d70c981f2681177
Yuliya_Hancharenka <Yuliya_Hancharenka@epam.com>
2017-05-23 18:03:57 +0300

    Add 3 uncompleted tasks

---
+++ src/hakerrank/java/strings/TagContentExtractor.java
@@ -0,0 +1,22 @@
+package hakerrank.java.strings;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class TagContentExtractor {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String line = in.nextLine();
+
+            //Write your code here
+
+            testCases--;
+        }
+    }
+}
