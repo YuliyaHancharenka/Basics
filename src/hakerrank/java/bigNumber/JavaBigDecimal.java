ef0a5738fdb2af203df0b4a72d1969ba166d5b1f
Yuliya_Hancharenka <Yuliya_Hancharenka@epam.com>
2017-05-26 16:52:27 +0300

    Add JavaBigDecimal task Add Main task (strings)

--- src/hakerrank/java/bigNumber/JavaBigDecimal.java
+++ src/hakerrank/java/bigNumber/JavaBigDecimal.java
@@ -15,8 +15,8 @@ public class JavaBigDecimal {
         sc.close();
 
         //Sorting (Selection)
-        for (int i = 0; i < (s.length - 1); i++) {
-            for (int k = (i + 1); k < s.length; k++) {
+        for (int i = 0; i < (s.length - 2); i++) {
+            for (int k = (i + 1); k < s.length - 2; k++) {
                 if (new BigDecimal(s[i]).compareTo(new BigDecimal(s[k])) < 0) {
                     String tempValue = s[i];
                     s[i] = s[k];
