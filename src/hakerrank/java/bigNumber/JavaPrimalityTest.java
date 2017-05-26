6439dda89631f2b6f0b7a97c747bf664fabe1ef4
Yuliya_Hancharenka <Yuliya_Hancharenka@epam.com>
2017-05-26 17:01:18 +0300

    Add JavaPrimalityTestl task

--- src/hakerrank/java/bigNumber/JavaPrimalityTest.java
+++ src/hakerrank/java/bigNumber/JavaPrimalityTest.java
@@ -5,12 +5,14 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class JavaPrimalityTest {
+
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         BigInteger n = in.nextBigInteger();
-        in.close();
-        // Write your code here.
 
+        System.out.println(n.isProbablePrime(100) ? "prime" : "not prime");
+        in.close();
 
     }
 }
