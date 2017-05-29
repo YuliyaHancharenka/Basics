f06e4c5bd006366cb4d346664d04edf5c1b09fdc
Yuliya_Hancharenka <Yuliya_Hancharenka@epam.com>
2017-05-29 14:37:27 +0300

    Add JavaSubarray completed task

--- src/hakerrank/java/dataStructures/JavaSubarray.java
+++ src/hakerrank/java/dataStructures/JavaSubarray.java
@@ -1,5 +1,35 @@
 package hakerrank.java.dataStructures;
 
 
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class JavaSubarray {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        IntStream.range(0, n).forEach(i -> a[i] = in.nextInt());            // fill array, instead of FOR loop
+
+        int count = 0;
+        for (int shift = 0; shift < n; shift++)
+            for (int i = 0; i < n - shift; i++) {
+                if (isSumNegative(a, i, i + shift)) {
+                    count++;
+                }
+            }
+        System.out.println(count);
+    }
+
+    private static boolean isSumNegative(int[] a, int begin, int end) {
+        int sum = 0;
+        for (int i = begin; i <= end; i++) {
+            sum = sum + a[i];
+        }
+        if (sum < 0){
+            return true;
+        }
+        return false;
+    }
 }
