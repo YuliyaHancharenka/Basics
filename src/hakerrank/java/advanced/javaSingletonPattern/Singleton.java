dfd4683cb8ed1ac1e27087bc301b77a00866dd3b
Yuliya_Hancharenka <Yuliya_Hancharenka@epam.com>
2017-09-28 18:08:09 +0300

    Add advanced tasks

---
+++ src/hakerrank/java/advanced/javaSingletonPattern/Singleton.java
@@ -0,0 +1,15 @@
+package hakerrank.java.advanced.javaSingletonPattern;
+
+
+public class Singleton {
+    private Singleton() {
+    }
+
+    public String str;
+    private static Singleton instance = new Singleton();
+
+    static Singleton getSingleInstance() {
+        return instance;
+    }
+}
+
