de1e0b11ccc35108e52709033e8ce75046094ead
Yuliya Hancharenka <Yuliya_Hancharenka@epam.com>
2018-02-09 01:32:02 +0300

    Add diff

---
+++ src/diffTasks/StringReverse.java
@@ -0,0 +1,13 @@
+package diffTasks;
+
+public class StringReverse {
+
+    public static void main(String[] args) {
+        String s = "I am the better Automation Tester in EPAM";
+        StringBuffer sb = new StringBuffer();
+        for (int i = s.length() - 1; i >= 0; i--){
+            sb = sb.append(s.charAt(i));
+        }
+        System.out.println(sb.toString());
+    }
+}
