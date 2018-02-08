8d8085066f58d3d48c60620b6feb89c7aba9dae5
Yuliya Hancharenka <Yuliya_Hancharenka@epam.com>
2018-02-09 01:12:34 +0300

    Add Command

--- src/patterns/command/RemoteControl.java
+++ src/patterns/command/RemoteControl.java
@@ -10,7 +10,7 @@ public class RemoteControl {
         offCommands = new Command[7];
 
         Command noCommand = new NoCommand();
-        for(int i = 0; i < 7; i++){
+        for (int i = 0; i < 7; i++) {
             onCommands[i] = noCommand;
             offCommands[i] = noCommand;
         }
