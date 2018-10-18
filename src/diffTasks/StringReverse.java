2d20b25e2e2550b36e4eff4eedce41370c08fa28
Yuliya_Hancharenka <Yuliya_Hancharenka@epam.com>
2018-10-18 15:21:16 +0300

    Add String task

--- src/diffTasks/StringReverse.java
+++ src/diffTasks/StringReverse.java
@@ -2,6 +2,27 @@ package diffTasks;
 
 public class StringReverse {
 
+    public static void typeEachCharacterOneByOne(String value) {
+        String[] textToType = value.split("");
+        for (String letter : textToType) {
+            System.out.println(letter);
+        }
+    }
+
+    public static void typeTextByLetters(String text) {
+        char[] textToType = text.toCharArray();
+        for (char letter : textToType) {
+            String str = Character.toString(letter);
+            System.out.println(str);
+        }
+    }
+
+    public static void typeTextByLettersNew(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println(text.charAt(i));
+        }
+    }
+
     public static void main(String[] args) {
         String s = "I am the better Automation Tester in EPAM";
         StringBuffer sb = new StringBuffer();
@@ -9,5 +30,9 @@ public class StringReverse {
             sb = sb.append(s.charAt(i));
         }
         System.out.println(sb.toString());
+
+        typeEachCharacterOneByOne("text");
+        typeTextByLetters("text");
+        typeTextByLettersNew("text");
     }
 }
